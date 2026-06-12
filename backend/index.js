fd20c33e1720f88bbe9c35ee16cc6e85ff777429
Riya Dutta <riyapc1406@gmail.com>
2026-06-12 12:53:43 +0530

    added health check route

--- backend/index.js
+++ backend/index.js
@@ -68,6 +68,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/",(req,res)=>{
+  res.status(200).json({message:"ok"})
+})
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listings", listingRouter);
