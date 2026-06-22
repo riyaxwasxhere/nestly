9cdf6f83368d8da08b8514ea3e811cf503ac7aa1
Riya Dutta <riyapc1406@gmail.com>
2026-06-22 21:23:41 +0530

    ..

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -133,7 +133,7 @@ export const googleSignin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000
     });
     res.status(200).json({ message: "Google signin successful", user });
